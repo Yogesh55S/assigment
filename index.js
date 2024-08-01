6b2e2ef065294d3d91cdec9ed99efa3ffb1da660
Yogesh55S <yogesharma914@gmail.com>
2024-08-01 23:35:36 +0530

    some changes

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const URL = require('./models/url');
 const PORT = 3000;
 const urlRoute = require("./routes/url");
 const {connectTomongoDB} = require("./connect");
+const path = require('path');
+
+app.set("view engine","ejs");
+app.set('views',path.resolve("./views"));
+app.get("/test",async(req,res)=>{
+  const allUrls = await URL.find({});
+  return res.render('home');
+});
 
 connectTomongoDB("mongodb://localhost:27017/short-url")
 .then(()=> console.log("connected succesfully"));
 
 app.use(express.json());
+
+app.use("/url",urlRoute);  
+
 app.get('/:shortid',async (req,res)=>{
    const shortid = req.params.shortid;
    const entry = await URL.findOneAndUpdate(
@@ -24,5 +35,5 @@ app.get('/:shortid',async (req,res)=>{
    );
    res.redirect(entry.redirectURL);
 })
-app.use("/url",urlRoute);  
+
 app.listen(PORT,() => console.log(`Server Started at port: ${PORT}`))     
