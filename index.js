471a39bb3f0d1951212a33ebb765a13720a88ba1
Yogesh55S <yogesharma914@gmail.com>
2024-08-05 23:25:13 +0530

    some changes

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const URL = require('./models/url');
 const PORT = 3000;
 const urlRoute = require("./routes/url");
+const staticrout = require("./routes/staticrouter");
 const {connectTomongoDB} = require("./connect");
 const path = require('path');
 
 app.set("view engine","ejs");
 app.set('views',path.resolve("./views"));
-app.get("/test",async(req,res)=>{
+/*app.get("/test",async(req,res)=>{
   const allUrls = await URL.find({});
-  return res.render('home');
-});
+  return res.render('home',{
+    urls: allUrls,
+    name: 'yogesh'
+  });
+});*/
+
+
 
 connectTomongoDB("mongodb://localhost:27017/short-url")
 .then(()=> console.log("connected succesfully"));
 
 app.use(express.json());
-
+app.use(express.urlencoded({extended : false}))
 app.use("/url",urlRoute);  
-
-app.get('/:shortid',async (req,res)=>{
+app.use("/",staticrout);
+app.get('/url/:shortid ',async (req,res)=>{
    const shortid = req.params.shortid;
    const entry = await URL.findOneAndUpdate(
   {
